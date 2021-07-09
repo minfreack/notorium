550d19727fb2655c32c848d21d2305c228011ca7
Ernesto <neto2racas@gmail.com>
2021-07-09 15:43:31 -0500

    Login button

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./login.css";
+import "./login.scss";
 
 export const Login = () => {
   return (
@@ -7,7 +7,7 @@ export const Login = () => {
       <div class="switch-button">
         <input class="switch-button-checkbox" type="checkbox"></input>
         <label class="switch-button-label" for="">
-          <span class="switch-button-label-span">Photo</span>
+          <span class="switch-button-label-span">Registrarse</span>
         </label>
       </div>
     </div>
